79c25bf6be57d7855776f9d6d7ff2868541f7c74
uiwwnw <uiwwnw@icloud.com>
2019-07-14 15:13:20 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,31 @@
 <template>
-  <div class="app">
-    <hello-world class="app__hello"
-      title="Welcome to your Vue MDC Adapter App"
-      :vmaLinks="vmaLinks"
-      :mdcLinks="mdcLinks">
-    </hello-world>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
 
-  export default {
-    data () {
-      return {
-        vmaLinks: [
-          {
-            title: 'Documentation',
-            url: 'https://stasson.github.io/vue-mdc-adapter'
-          },
-          {
-            title: 'GitHub',
-            url: 'https://github.com/stasson/vue-mdc-adapter'
-          },
-          {
-            title: 'Twitter',
-            url: 'https://twitter.com/vuemdc'
-          },
-          {
-            title: 'Chat',
-            url: 'https://gitter.im/vue-mdc-adapter/Lobby'
-          }
-        ],
-        mdcLinks: [
-          {
-            title: 'Documentation',
-            url: 'https://material.io/components/web/'
-          },
-          {
-            title: 'GitHub',
-            url: 'https://github.com/material-components/material-components-web'
-          },
-          {
-            title: 'Guidelines',
-            url: 'https://material.io/guidelines'
-          },
-          {
-            title: 'Awesome Material Components',
-            url: 'https://github.com/webdenim/awesome-material-components'
-          }
-        ]
-      }
-    },
-    components: { HelloWorld }
-  }
+export default {
+  name: 'app',
+};
 </script>
 
-<style lang="scss">
-  // First, set the value for variable
-  $mdc-typography-font-family: "Roboto Mono", monospace;
-
-  // Then, import required files
-  @import "@material/typography/mixins";
-
-  html {
-    width: 100%;
-    height: 100%;
-  }
-
-  body {
-    @include mdc-typography(body2);
-
-    width: 100%;
-    min-height: 100%;
-    margin: 0;
-  }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
